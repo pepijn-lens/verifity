577c4517307150f47146775a25c0eed486f53a35
pepijn-lens <129872939+pepijn-lens@users.noreply.github.com>
2026-03-05 14:38:29 +0100

    Remove Firebase configuration from firebase.js
    Removed Firebase configuration details from firebase.js.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,15 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB3lnABSQeuzvdNe5pAReA2feiGEW-gR9A",
-  authDomain: "verifity-ai.firebaseapp.com",
-  projectId: "verifity-ai",
-  storageBucket: "verifity-ai.firebasestorage.app",
-  messagingSenderId: "498079762864",
-  appId: "1:498079762864:web:bf11840a656f03a0202e75",
-  measurementId: "G-W71DMXMKKR",
-};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
